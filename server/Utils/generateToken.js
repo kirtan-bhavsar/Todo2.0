a272fb97af13da28fb1cbfcb1d45b3049d08755a
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-05-13 23:58:07 +0530

    added_generateToken_cookieOptionsUpdated_Dt:13052025

--- server/Utils/generateToken.js
+++ server/Utils/generateToken.js
@@ -11,6 +11,8 @@ const generateToken = async (res, user) => {
   const cookieOptions = {
     httpOnly: true,
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    secure: true,
+    sameSite: "None",
     // expires: new Date(Date.now() + 100 * 1000),
   };
 
